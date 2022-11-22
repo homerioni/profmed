7e41ceb6ba87e282e4c8c5745b07d15d4740e157
Артем Горбаль <homerion@MacBook-Air-Artem.local>
2022-11-22 08:59:58 +0300

    up

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ $(document).ready(function () {
 
     // Modal
     $('.modal .close').click(function () {
-        $('body').removeClass('lock').css('width', 'auto');
+        $('body').removeClass('lock').width('');
+        $('header').width('');
         $(this).parents('.modal').fadeOut();
     });
     $('.form-btn').click(function () {
@@ -47,7 +48,8 @@ $(document).ready(function () {
 
     // Modal request
     $('.popup-request').click(function () {
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-request').css('display', 'flex').hide().fadeIn();
     });
     $('.modal-request .close').click(function () {
@@ -62,7 +64,8 @@ $(document).ready(function () {
 
     // Modal help
     $('.popup-help').click(function () {
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-help').css('display', 'flex').hide().fadeIn();
     });
     $('.modal-help .close').click(function () {
@@ -77,7 +80,8 @@ $(document).ready(function () {
 
     // Modal new review
     $('.add-review').click(function () {
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-new-review').css('display', 'flex').hide().fadeIn();
     });
     $('.modal-new-review .close').click(function () {
@@ -97,19 +101,22 @@ $(document).ready(function () {
     // Modal licenses
     $('.licenses__item').click(function () {
         licenses_modal_slider.slideTo($(this).index() + 1);
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-licenses').css('display', 'flex').hide().fadeIn();
     });
 
     // Modal intro
     $('.popup-intro').click(function () {
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-intro').css('display', 'flex').hide().fadeIn();
     });
 
     // Modal add comment
     $('.add-comment').click(function () {
-        $('body').css('width', $('body').width()).addClass('lock');
+        $('body').width($('body').width()).addClass('lock');
+        $('header').width($('body').width());
         $('.modal-add-comment').css('display', 'flex').hide().fadeIn();
     });
     $('.modal-add-comment .close').click(function () {
